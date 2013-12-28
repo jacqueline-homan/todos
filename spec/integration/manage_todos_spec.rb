0d8d857cb1dd05797d4a6f0725be5a04b6d4e583
jacqueline-homan <jacquelinehoman7@gmail.com>
2013-12-28 01:50:51 -0500

    Minor refactoring for specs and ensure todos are accessed from current_user

--- spec/integration/manage_todos_spec.rb
+++ spec/integration/manage_todos_spec.rb
@@ -2,14 +2,25 @@ require 'spec_helper'
 
 feature 'Manage todos' do
   scenario 'create new todo' do
-  	visit root_path
-  	fill_in 'Email address', with: 'person@example.com'
-  	click_button 'Sign In'
+  	sign_in
   	click_link 'Add new todo'
   	fill_in 'Description', with: 'Read three chapters of RSpec book'
   	click_button 'Create todo'
   	expect(page).to have_css 'li.todo', text: 'Read three chapters of RSpec book'
   end
+## The following def block was moved into a module
+## to keep our features nice and slim. 
+## The logic for signing in is in a module
+## that can be managed separately, so if the logic for
+## signing in changes, it's all contained in the module.
+ 
+
+#  def sign_in
+#  	visit root_path
+#  	fill_in 'Email address', with: 'person@email.com'
+#  	click_button 'Sign In'
+#  end
+
 end
 
 	
