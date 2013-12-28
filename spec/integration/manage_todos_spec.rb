1a70abe3fe2a8dbaa4cc46de2a3c9e37e1d0add1
jacqueline-homan <jacquelinehoman7@gmail.com>
2013-12-28 02:56:41 -0500

    added user_spec.rb file

--- spec/integration/manage_todos_spec.rb
+++ spec/integration/manage_todos_spec.rb
@@ -8,6 +8,16 @@ feature 'Manage todos' do
   	click_button 'Create todo'
   	expect(page).to have_css 'li.todo', text: 'Read three chapters of RSpec book'
   end
+
+  scenario 'view only my todos' do
+    Todo.create(description: 'Buy coffee cream', owner_email: 'not_me@example.com')
+    sign_in_as 'me@example.com'
+    click_link 'Add new todo'
+  	fill_in 'Description', with: 'Read three chapters of RSpec book'
+  	click_button 'Create todo'
+  	expect(page).to have_css 'li.todo', text: 'Read three chapters of RSpec book'
+  	expect(page).not_to have_css 'li.todo', text: 'Buy coffee cream'
+  end
 ## The following def block was moved into a module
 ## to keep our features nice and slim. 
 ## The logic for signing in is in a module
@@ -20,7 +30,6 @@ feature 'Manage todos' do
 #  	fill_in 'Email address', with: 'person@email.com'
 #  	click_button 'Sign In'
 #  end
-
 end
 
 	
